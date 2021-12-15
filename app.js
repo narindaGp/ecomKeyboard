e25bdd6661947dc0438356eae333c7c226dfe562
Mahardikadwiputra27 <mahardikadwiputra27@gmail.com>
2021-12-15 23:11:20 +0700

    v1

---
+++ app.js
@@ -0,0 +1,6 @@
+/*
+npx sequelize-cli model:generate --name seller --attributes sellerName:string,email:string,password:string,address:string,phoneNumber:string,money:integer
+npx sequelize-cli model:generate --name product --attributes productName:string,price:integer,stock:integer,SellerId:integer
+npx sequelize-cli model:generate --name customer --attributes customerName:string,email:string,password:string,address:string,phoneNumber:string,money:integer
+
+*/
---
+++ migrations/20211215160944-create-product.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      productName: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      stock: {
+        type: Sequelize.INTEGER
+      },
+      SellerId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('products');
+  }
+};
