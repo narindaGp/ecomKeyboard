fd25c6cbdbcb8da79426056161214d462caa130d
Mahardikadwiputra27 <mahardikadwiputra27@gmail.com>
2021-12-16 07:41:56 +0700

    startMigrationJsonRoutesController

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ npx sequelize-cli model:generate --name product --attributes productName:string,
 npx sequelize-cli model:generate --name customer --attributes customerName:string,email:string,password:string,address:string,phoneNumber:string,money:integer
 
 */
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+const routes = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
+app.use(routes)
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+const customerRoute = require('./customer')
+const productRoute = require('./product')
+const sellerRoute = require('./seller')
+
+
+
+router.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+//   router.get('/', (req, res) => {
+//     res.render('marketplace')
+//   })
+router.use('/customer', customerRoute)
+router.use('/product', productRoute)
+router.use('/seller', sellerRoute)
+
+
+module.exports = router
